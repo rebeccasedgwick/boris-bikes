843704c4201211e8d8ceadb05acf6008cae6f452
Andrew Wood <andrew.wood.dev@outlook.com>
2018-08-07 16:48:47 +0100

    added has_bike and @bikes to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,23 @@
 require "bike"
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+    @bikes << bike
+    self
+  end
+
+  def has_bike?
+    @bikes.empty? == true ? (return false) : (return true)
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,29 @@ describe DockingStation do
   it "releases a working bike" do
     expect(subject.release_bike.working?).to eq true
   end
+  it "responds to dock_bike" do
+    expect(subject).to respond_to :dock_bike
+  end
+  # it "docks a bike" do
+  #   expect(subject.dock_bike(Bike.new)).to eq true
+  # end
+  it "responds to has_bike" do
+    expect(subject).to respond_to :has_bike?
+  end
+  it "responds true to has_bike as true once bike docked" do
+    expect(subject.dock_bike(Bike.new).has_bike?).to eq true
+  end
+  it "responds false to has_bike when a new station" do
+    expect(subject.has_bike?).to eq false
+  end
 end
+
+# describe DockingStation do
+#   before do
+#
+#
+#   end
+#   it "does stuff"
+#     expects
+#   end
+# end
