f8b90aa271785310ebfe2292229e15c50c7c8f2c
Andrew Wood <andrew.wood.dev@outlook.com>
2018-08-07 15:49:43 +0100

    Created bikes and working / release methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
   it "responds to release_bike" do
     expect(subject).to respond_to :release_bike
+  end
+  it "releases a bike" do
+    expect(subject.release_bike).to be_a Bike
+  end
+  it "releases a working bike" do
+    expect(subject.release_bike.working?).to eq true
   end
 end
